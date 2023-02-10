692aaa6f197d59ee9117f9d194547cfd37146fde
Cyrah Sophia Angella Cuenca <119916620+i675nrk@users.noreply.github.com>
2023-02-10 20:31:09 +0800

    Add files via upload

---
+++ exercise_5/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    
+    private String name = "Sophia";
+
+    // get() method
+    public String getName(){
+        return this.name;
+    }
+
+    //set() method
+    public void setName(String personName){
+        this.name = personName;
+    }
+}
